6d292a93f6c1f40f04bae6b91a20f66cd02a67f6
nonoro <kgu1022@naver.com>
2022-05-31 16:56:07 +0900

    feat(ch16 소켓프로그래밍) : 22.05.31 중간저장2

---
+++ src/ch16/socketprogramming/TcpIpMultichatServer.java
@@ -0,0 +1,13 @@
+//package ch16.socketprogramming;
+//
+//import java.util.*;
+//
+//class TcpIpMultichatServer {
+//    HashMap clientts;
+//
+//    TcpIpMultichatServer() {
+////        clients = new HashMap();
+//        Collections.synchronizedMap(clientts);
+//
+//    }
+//}
